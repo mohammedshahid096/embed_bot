765f12906c5ef83798acfeadcf0a6afdf330b6fe
Mohammed shahid <84904302+mohammedshahid096@users.noreply.github.com>
2026-07-13 14:59:39 +0530

    added organisation details in the schema

--- backend/src/schema/organisation.model.ts
+++ backend/src/schema/organisation.model.ts
@@ -1,11 +1,20 @@
-import mongoose, { Model, Schema } from "mongoose";
+import mongoose, { Document, Model, Schema } from "mongoose";
 import modelConstants from "../constants/model.constant";
 
-export interface OrganizationInterface {
-  email?: string;
-  website: string;
+export interface OrganizationInterface extends Document {
   name: string;
+  email: string;
+  website: string;
   isActive: boolean;
+  address: {
+    street: string;
+    city: string;
+    state: string;
+    country: string;
+    zipCode: string;
+  };
+  contact: string;
+  description: string;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -14,8 +23,22 @@ const organizationSchema = new Schema<OrganizationInterface>(
   {
     name: { type: String, required: true },
     website: { type: String, required: true },
-    email: { type: String },
+    email: { type: String, required: true },
     isActive: { type: Boolean, default: true },
+    address: {
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      country: { type: String, required: true },
+      zipCode: { type: String, required: true },
+    },
+    contact: { type: String, required: true },
+    description: {
+      type: String,
+      minLength: 50,
+      maxLength: 500,
+      required: true,
+    },
   },
   { timestamps: true },
 );
